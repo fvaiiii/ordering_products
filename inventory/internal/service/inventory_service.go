922b36efc35b4003996a9aa285d4b8fcff9f7e14
daria <m2403815@edu.misis.ru>
2025-11-29 15:20:05 +0300

    implement inventory_service

--- inventory/internal/service/inventory_service.go
+++ inventory/internal/service/inventory_service.go
@@ -1,1 +1,37 @@
 package service
+
+import (
+	"context"
+
+	"github.com/fvaiiii/ordering_products/inventory/internal/models"
+	"github.com/fvaiiii/ordering_products/inventory/internal/port/repo"
+)
+
+type InventoryService struct {
+	repo repo.Products
+}
+
+func NewInventoryService(repo repo.Products) *InventoryService {
+	return &InventoryService{
+		repo: repo,
+	}
+}
+
+func (i *InventoryService) GetProduct(ctx context.Context, productID string) (*models.Product, error) {
+	product, err := i.repo.GetProduct(ctx, productID)
+	if err != nil {
+		return nil, err
+	}
+
+	return product, nil
+}
+
+func (i *InventoryService) ListProducts(ctx context.Context, filter models.ProductsFilter) ([]*models.Product, error) {
+	products, err := i.repo.ListProducts(ctx, filter)
+	if err != nil {
+		return nil, err
+	}
+
+	return products, nil
+
+}
