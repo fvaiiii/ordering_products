08ca2e6971363218fedf760467506c2d8dec9f06
daria <m2403815@edu.misis.ru>
2025-12-08 17:01:45 +0300

    implementation payment client interface

---
+++ order/internal/clients/payment_client.go
@@ -0,0 +1,49 @@
+package clients
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"github.com/fvaiiii/ordering_products/order/internal/repo"
+	paymentv1 "github.com/fvaiiii/ordering_products/shared/pkg/proto/payment/v1"
+	"google.golang.org/grpc"
+)
+
+var _ = (*repo.PaymentClient)(nil)
+
+type PaymentClient struct {
+	conn   *grpc.ClientConn
+	client paymentv1.PaymentServiceClient
+}
+
+func NewPaymentClient(conn *grpc.ClientConn) *PaymentClient {
+	return &PaymentClient{
+		conn:   conn,
+		client: paymentv1.NewPaymentServiceClient(conn),
+	}
+}
+func (c *PaymentClient) Close() error {
+	return c.conn.Close()
+}
+
+func (c *PaymentClient) PayOrder(ctx context.Context, orderUUID, userUUID string, paymentMethod paymentv1.PaymentMethod) (string, error) {
+
+	req := &paymentv1.PaymentRequest{
+		OrderUuid:     orderUUID,
+		UserUuid:      userUUID,
+		PaymentMethod: paymentMethod,
+	}
+
+	resp, err := c.client.PayOrder(ctx, req)
+	if err != nil {
+		return "", fmt.Errorf("payment service error: %w", err)
+	}
+
+	if resp.GetTransactionUuid() == "" {
+		return "", errors.New("payment service returned empty transaction uuid")
+	}
+
+	return resp.GetTransactionUuid(), nil
+
+}
