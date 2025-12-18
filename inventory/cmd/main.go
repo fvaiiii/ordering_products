fce4951de87f7220e22cdf680e45cb3f6e89c64f
daria <m2403815@edu.misis.ru>
2025-12-19 01:54:42 +0300

    run inventory and payment servers

--- inventory/cmd/main.go
+++ inventory/cmd/main.go
@@ -1,1 +1,29 @@
-package cmd
+package main
+
+import (
+	"log"
+	"net"
+
+	grpcapi "github.com/fvaiiii/ordering_products/inventory/internal/api/grpc"
+	"github.com/fvaiiii/ordering_products/inventory/internal/repository"
+	"github.com/fvaiiii/ordering_products/inventory/internal/service"
+	v1 "github.com/fvaiiii/ordering_products/shared/pkg/proto/inventory/v1"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	repo := repository.NewProductsRepo()
+	svc := service.NewInventoryService(repo)
+	handler := grpcapi.NewServer(svc)
+
+	grpcServer := grpc.NewServer()
+	v1.RegisterInventoryServiceServer(grpcServer, handler)
+
+	lis, _ := net.Listen("tcp", ":50051")
+	log.Printf("Starting InventoryService on :50051")
+
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatal(err)
+	}
+
+}
